cfcc89c48a6dc596f8317542da07985da5034475
K.Mahfoudh <kerbouche.mahfoudh@gmail.com>
2019-05-10 23:48:43 +0100

    fixed the navbar animation, and added the main title (Switch) to the index page

--- js/main.js
+++ js/main.js
@@ -15,19 +15,19 @@ function scrollDetect(){
 
    $(document).on('scroll',function(){
        var newScrollValue = $(this).scrollTop();
-       if(newScrollValue === 0 && noRepeat === true){
-           $('#main-nav-list').removeClass('color-custom');
+       if(newScrollValue === 0 && noRepeat === true){ //in case we reach the top, we change navbar color from white to transparent
+           $('#main-nav-list').delay(3000).removeClass('color-custom').addClass('color-transparent');
            $('.custom-font-color').css('color','white');
        }
        if(newScrollValue> scrollValue){ // we're sliding down
            if(noRepeat === true){
-           $('#main-nav-list').addClass('hidden').removeClass('bg-custom'); /*this line is for making navbar fadeOut since fadeOut() didn't work for reasons that i ignore, so i used css transition to make it work*/
+           $('#main-nav-list').removeClass('color-transparent').addClass('hide'); /*this line is for making navbar fadeOut since fadeOut() didn't work for reasons that i ignore, so i used css transition to make it work*/
            //this one is for avoiding doing the action again
            noRepeat = false;
            }
        }else{
            if(noRepeat=== false){ // we're sliding up
-            $('#main-nav-list').removeClass('hidden').addClass('color-custom'); /*making navbar appear again and making it's background color white*/
+            $('#main-nav-list').removeClass('hide').addClass('color-custom'); /*making navbar appear again and making it's background color white*/
             $('.custom-font-color').css('color','black'); //this one is for changing bootstrap default color to black
             noRepeat = true;  //this one is for avoiding doing the action again
            }
